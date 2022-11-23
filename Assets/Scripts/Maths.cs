3536ce34d58ca7f82b1858f3e202fad4e95a6d71
desouza.paatrice@gmail.com <desouza.paatrice@gmail.com>
2022-11-23 02:48:52 +0100

    feat : Dot product challenge solved

--- Assets/Scripts/Maths.cs
+++ Assets/Scripts/Maths.cs
@@ -24,39 +24,48 @@ public class Maths : MonoBehaviour
         Debug.Log(" Unity Distance " + Vector3.Distance(_mosquitoPosition, _babyPosition));
     }
 
-     void CalculateAngle()
+    void CalculateAngle()
     {
-        // Pour calculer le Cos -1, vous disposer de la méthode  Mathf.ACos()
         // Vecteur directeur du gameObject moustique
-        Vector3 mosquitoDirectorVector =  this.transform.up;
-        // Pour le calcul de la norme d'un vecteur utilisez la propriété magnitude. Exemple : _babyPosition.magnitude;
-
+        Vector3 mosquitoDirectorVector = this.transform.up;
         // Vecteur allant du moustique vers le bébé
         Vector3 targetVector = _baby.transform.position - this.transform.position;
+        
+        // il n'est pas neccessaire dans le présent cas d'ajouter les coordonnées de l'axe z puisque nous travaillons dans un repère 2D
+        float dotProduct = (mosquitoDirectorVector.x * targetVector.x) + (mosquitoDirectorVector.y * targetVector.y);       
+        float angle = Mathf.Acos(dotProduct / (mosquitoDirectorVector.magnitude * targetVector.magnitude));
+        Debug.Log("angle : " + angle * Mathf.Rad2Deg);
+        Debug.Log(" Unity's angle : " + Vector3.Angle(mosquitoDirectorVector, targetVector));
 
-        Debug.Log("Donnez moi l'angle d'attack :) ! ");
+        Debug.Log($"Unity's dot product : {Vector3.Dot(mosquitoDirectorVector, targetVector)} Versus our dot product {dotProduct} ");
+        
+        //option de debogage pour visualiser les vecteurs
+        Debug.DrawRay(this.transform.position, mosquitoDirectorVector * 5, Color.yellow, 5);
+        Debug.DrawRay(this.transform.position, targetVector, Color.blue, 5);
     }
 
-    // Update is called once per frame
+// Update is called once per frame
     void Update()
-    {
-        // detecte et quantifie la pression exercée sur les touches directionnelles
-        // les valeurs oscillants entre -1 et 1
-        float translation = Input.GetAxis("Vertical") * _speed;
-        float rotation = Input.GetAxis("Horizontal") * _rotationSpeed;
-
-        // Time.deltaTime rend les mouvements plus fluides en utilisant une échelle 
-        //par seconde plutôt que par frame 
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-
-        transform.Translate(0, translation, 0);
-        transform.Rotate(0, 0, rotation);
-
-        if (Input.GetKeyDown(KeyCode.Space)) // execute les différentes méthodes si la touche "Espace" est appuyée
         {
-            //CalculateDistance(); Désactivons pour le moment cette méthode
-            CalculateAngle();
-        }
+            // detecte et quantifie la pression exercée sur les touches directionnelles
+            // les valeurs oscillants entre -1 et 1
+            float translation = Input.GetAxis("Vertical") * _speed;
+            float rotation = Input.GetAxis("Horizontal") * _rotationSpeed;
+
+            // Time.deltaTime rend les mouvements plus fluides en utilisant une échelle 
+            //par seconde plutôt que par frame 
+            translation *= Time.deltaTime;
+            rotation *= Time.deltaTime;
+
+            transform.Translate(0, translation, 0);
+            transform.Rotate(0, 0, rotation);
+
+            if (Input.GetKeyDown(KeyCode.Space)) // execute les différentes méthodes si la touche "Espace" est appuyée
+            {
+                //CalculateDistance(); Désactivons pour le moment cette méthode
+                CalculateAngle();
+            }
     }
+     
 }
+
