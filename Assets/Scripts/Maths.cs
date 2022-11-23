239fc38588abd7c97e17b573c2c5b48ee07baba4
desouza.paatrice@gmail.com <desouza.paatrice@gmail.com>
2022-11-23 19:28:18 +0100

    feat : Add cross product challenges

--- Assets/Scripts/Maths.cs
+++ Assets/Scripts/Maths.cs
@@ -10,7 +10,7 @@ public class Maths : MonoBehaviour
     Vector3 _mosquitoPosition;
     Vector3 _babyPosition;
 
-    void CalculateDistance()
+    float CalculateDistance()
     {
         // recupère les coordonnées de position
         _mosquitoPosition = this.transform.position;
@@ -18,10 +18,7 @@ public class Maths : MonoBehaviour
 
         float distance = Mathf.Sqrt(
                             Mathf.Pow((_babyPosition.x - _mosquitoPosition.x), 2) + Mathf.Pow((_babyPosition.y - _mosquitoPosition.y), 2));
-
-        Debug.DrawLine(this.transform.position, _babyPosition, Color.white, 10); // affiche pendant 10sec un vecteur de couleur blanche du moustique au bébé
-        Debug.Log(" Calcul Distance " + distance);
-        Debug.Log(" Unity Distance " + Vector3.Distance(_mosquitoPosition, _babyPosition));
+        return distance;
     }
 
     void CalculateAngle()
@@ -30,42 +27,38 @@ public class Maths : MonoBehaviour
         Vector3 mosquitoDirectorVector = this.transform.up;
         // Vecteur allant du moustique vers le bébé
         Vector3 targetVector = _baby.transform.position - this.transform.position;
-        
-        // il n'est pas neccessaire dans le présent cas d'ajouter les coordonnées de l'axe z puisque nous travaillons dans un repère 2D
-        float dotProduct = (mosquitoDirectorVector.x * targetVector.x) + (mosquitoDirectorVector.y * targetVector.y);       
-        float angle = Mathf.Acos(dotProduct / (mosquitoDirectorVector.magnitude * targetVector.magnitude));
-        Debug.Log("angle : " + angle * Mathf.Rad2Deg);
-        Debug.Log(" Unity's angle : " + Vector3.Angle(mosquitoDirectorVector, targetVector));
 
-        Debug.Log($"Unity's dot product : {Vector3.Dot(mosquitoDirectorVector, targetVector)} Versus our dot product {dotProduct} ");
-        
+        // il n'est pas neccessaire dans le présent cas d'ajouter les coordonnées de l'axe z puisque nous travaillons dans un repère 2D
+        float dotProduct = (mosquitoDirectorVector.x * targetVector.x) + (mosquitoDirectorVector.y * targetVector.y);
+        float angle = Mathf.Acos(dotProduct / (mosquitoDirectorVector.magnitude * targetVector.magnitude));
+
         //option de debogage pour visualiser les vecteurs
         Debug.DrawRay(this.transform.position, mosquitoDirectorVector * 5, Color.yellow, 5);
         Debug.DrawRay(this.transform.position, targetVector, Color.blue, 5);
     }
 
-// Update is called once per frame
+
+    // Update is called once per frame
     void Update()
+    {
+        // detecte et quantifie la pression exercée sur les touches directionnelles
+        // les valeurs oscillants entre -1 et 1
+        float translation = Input.GetAxis("Vertical") * _speed;
+        float rotation = Input.GetAxis("Horizontal") * _rotationSpeed;
+
+        // Time.deltaTime rend les mouvements plus fluides en utilisant une échelle 
+        //par seconde plutôt que par frame 
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, translation, 0);
+        transform.Rotate(0, 0, rotation);
+
+        if (Input.GetKeyDown(KeyCode.Space)) // execute les différentes méthodes si la touche "Espace" est appuyée
         {
-            // detecte et quantifie la pression exercée sur les touches directionnelles
-            // les valeurs oscillants entre -1 et 1
-            float translation = Input.GetAxis("Vertical") * _speed;
-            float rotation = Input.GetAxis("Horizontal") * _rotationSpeed;
-
-            // Time.deltaTime rend les mouvements plus fluides en utilisant une échelle 
-            //par seconde plutôt que par frame 
-            translation *= Time.deltaTime;
-            rotation *= Time.deltaTime;
-
-            transform.Translate(0, translation, 0);
-            transform.Rotate(0, 0, rotation);
-
-            if (Input.GetKeyDown(KeyCode.Space)) // execute les différentes méthodes si la touche "Espace" est appuyée
-            {
-                //CalculateDistance(); Désactivons pour le moment cette méthode
-                CalculateAngle();
-            }
+            //Debug.Log(" Distance : " + CalculateDistance());
+            CalculateAngle();
+        }
     }
-     
-}
 
+}
