5592b4b9db7478d8cbbe5fe8fb5d48f83e6f07e1
PatFantasia <desouza.paatrice@gmail.com>
2023-03-28 17:49:20 +0100

    feat: Update code solution

--- Assets/Scripts/Maths.cs
+++ Assets/Scripts/Maths.cs
@@ -1,45 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Maths : MonoBehaviour
 {
-    public GameObject _baby;
-    float _speed = 8.0f;
-    float _rotationSpeed = 100f;
-    Vector3 _mosquitoPosition;
-    Vector3 _babyPosition;
+    // [SerializeField] : Permet d'acc√©der √† une variable priv√©e depuis l'inspector
+    [SerializeField] private GameObject _baby;
+    private float _speed = 8.0f;
+    private float _rotationSpeed = 100f;
+    private Vector3 _mosquitoPosition;
+    private Vector3 _babyPosition;
 
-    void CalculDistance()
+    private void CalculDistance()
     {
-        // recupËre les coordonnÈes de position
+        // recup√®re les coordonn√©es de position
         _mosquitoPosition = this.transform.position;
         _babyPosition = _baby.transform.position;
 
         float distance = Mathf.Sqrt(
                             Mathf.Pow((_babyPosition.x - _mosquitoPosition.x), 2) + Mathf.Pow((_babyPosition.y - _mosquitoPosition.y), 2));
 
-        Debug.DrawLine(this.transform.position, _babyPosition, Color.white, 10); // affiche pendant 10sec un vecteur de couleur blanche du moustique au bÈbÈ
+        Debug.DrawLine(this.transform.position, _babyPosition, Color.white, 10); // affiche pendant 10 secondes un vecteur directeur de couleur blanche du moustique au b√©b√©
         Debug.Log(" Calcul Distance " + distance);
         Debug.Log(" Unity Distance " + Vector3.Distance(_mosquitoPosition, _babyPosition));
     }
-    // Update is called once per frame
-    void Update()
+    
+    // methode native de unity s'ex√©cutant une fois par frame
+    private void Update()
     {
-        // detecte et quantifie la pression exercÈe sur les touches directionnelles
+        // detecte et quantifie la pression exerc√©e sur les touches directionnelles
         // les valeurs oscillants entre -1 et 1
         float translation = Input.GetAxis("Vertical") * _speed;
         float rotation = Input.GetAxis("Horizontal") * _rotationSpeed;
 
-        // Time.deltaTime rend les mouvements plus fluides en utilisant une Èchelle 
-        //par seconde plutÙt que par frame 
+        // Time.deltaTime rend les mouvements plus fluides en utilisant une √©chelle 
+        //par seconde plut√¥t que par frame 
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
 
         transform.Translate(0, translation, 0);
         transform.Rotate(0, 0, rotation);
 
-        if (Input.GetKeyDown(KeyCode.Space)) // lance la mÈthode CalculDistance si la touche "Espace" est appuyÈe
+        if (Input.GetKeyDown(KeyCode.Space)) // lance la m√©thode CalculDistance si la touche "Espace" est appuy√©e
         {
             CalculDistance();
         }
