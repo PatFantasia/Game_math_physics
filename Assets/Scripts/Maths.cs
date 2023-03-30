abc652c1237526347a2c9091fa68c0801bd4e8cf
PatFantasia <desouza.paatrice@gmail.com>
2023-03-30 13:26:24 +0100

    doc: Incorrect use on transform.Translate

--- Assets/Scripts/Maths.cs
+++ Assets/Scripts/Maths.cs
@@ -1,19 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Maths : MonoBehaviour
 {
-    public GameObject _baby;
-    float _speed = 8.0f;
-    float _rotationSpeed = 100f;
-    Vector3 _mosquitoPosition;
-    Vector3 _babyPosition;
-    bool _enableBiteAttack = false;
+    [SerializeField] private GameObject _baby;
+    private float _speed = 8.0f;
+    private float _rotationSpeed = 100f;
+    private Vector3 _mosquitoPosition;
+    private Vector3 _babyPosition;
+    private bool _enableBiteAttack = false;
 
-    float CalculateDistance()
+
+    private float CalculateDistance()
     {
-        // recupère les coordonnées de position
         _mosquitoPosition = this.transform.position;
         _babyPosition = _baby.transform.position;
 
@@ -22,18 +20,18 @@ public class Maths : MonoBehaviour
         return distance;
     }
 
-    void CalculateAngle()
+    private void CalculateAngle()
     {
         // Vecteur directeur du gameObject moustique
         Vector3 mosquitoDirectorVector = this.transform.up;
-        // Vecteur allant du moustique vers le bébé
+        // Vecteur allant du moustique vers le bÃ©bÃ©
         Vector3 targetVector = _baby.transform.position - this.transform.position;
 
-        // il n'est pas neccessaire dans le présent cas d'ajouter les coordonnées de l'axe z puisque nous travaillons dans un repère 2D
+        // il n'est pas neccessaire dans le prÃ©sent cas d'ajouter les coordonnÃ©es de l'axe z puisque nous travaillons dans un repÃ¨re 2D
         float dotProduct = (mosquitoDirectorVector.x * targetVector.x) + (mosquitoDirectorVector.y * targetVector.y);
         float angle = Mathf.Acos(dotProduct / (mosquitoDirectorVector.magnitude * targetVector.magnitude));
 
-        // si (V^W).z > 0 sens trignométrique direct sinon sensTrigonométrique indirecte
+        // si (V^W).z > 0 sens trignomÃ©trique direct sinon sensTrigonomÃ©trique indirecte
         int clockwise = CrossProduct(mosquitoDirectorVector, targetVector).z > 0 ? 1 : -1;
         this.transform.Rotate(0, 0, angle * Mathf.Rad2Deg * clockwise); // Effectue une rotation sur l'axe z
 
@@ -43,7 +41,7 @@ public class Maths : MonoBehaviour
         Debug.DrawRay(this.transform.position, mosquitoDirectorVector * 5, Color.yellow, 5);
         Debug.DrawRay(this.transform.position, targetVector, Color.blue, 5);
     }
-    Vector3 CrossProduct(Vector3 v, Vector3 w)
+    private Vector3 CrossProduct(Vector3 v, Vector3 w)
     {
         float xCoordinate = v.y * w.z - v.z * w.y;
         float yCoordinate = v.z * w.x - v.x * w.z;
@@ -51,35 +49,35 @@ public class Maths : MonoBehaviour
         Vector3 CrossProduct = new Vector3(xCoordinate, yCoordinate, zCoordinate);
         return CrossProduct;
     }
-    void BiteAttack()
-    {
+    private void BiteAttack()
+    { 
         CalculateAngle();
-        this.transform.Translate(0, CalculateDistance() * Time.deltaTime, 0);
+        this.transform.Translate(this.transform.up * _speed * Time.deltaTime, Space.World);
         float distance = CalculateDistance();
-        if (CalculateDistance() < 0.5) _enableBiteAttack = false; // si la distance < 0.5 f, _enableBiteAttack passe à  false, 
-    }                                                             // stoppant ainsi l'exécution de BiteAttack()
-    // Update is called once per frame
-    void Update()
+        float deadZoneLimit = 0.5f; // distance Ã  partir laquelle il n'est plus utile de lancer la mÃ©thode
+
+        if (distance < 0.5) _enableBiteAttack = false; // si la distance < 0.5 f, _enableBiteAttack passe Ã   false, 
+    }
+
+    private void Update()
     {
-        // detecte et quantifie la pression exercée sur les touches directionnelles
-        // les valeurs oscillants entre -1 et 1
         float translation = Input.GetAxis("Vertical") * _speed;
         float rotation = Input.GetAxis("Horizontal") * _rotationSpeed;
 
-        // Time.deltaTime rend les mouvements plus fluides en utilisant une échelle 
-        //par seconde plutôt que par frame 
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
 
         transform.Translate(0, translation, 0);
         transform.Rotate(0, 0, rotation);
-        if (Input.GetKeyDown(KeyCode.A)) // Si la touche "A" est appuyée.
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            _enableBiteAttack = !_enableBiteAttack; // _enableBiteAttack prend la valeur opposée à sa précédente valeur
+            _enableBiteAttack = !_enableBiteAttack; // _enableBiteAttack prend la valeur opposÃ©e Ã© sa prÃ©cÃ©dente valeur
         }
 
-        if (_enableBiteAttack) BiteAttack(); // execute BiteAttack si _enableBiteAttack = true
-        if (Input.GetKeyDown(KeyCode.Space)) // execute les différentes méthodes si la touche "Espace" est appuyée
+        if (_enableBiteAttack) 
+            BiteAttack();  
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             //Debug.Log(" Distance : " + CalculateDistance());   
             CalculateAngle();
